a4ebb5384169f51107436dd6b547399827fb3c82
Dhruv Ahuja <dhruvahuja2k@gmail.com>
2021-12-31 13:05:36 +0530

    feat: add spotipy reference code for songs

--- app/sp_experimental.py
+++ app/sp_experimental.py
@@ -1,7 +1,9 @@
 from collections import namedtuple
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
-from typing import List
+from dataclasses import dataclass
+
+# from typing import List
 
 
 # define scopes for the api
@@ -15,16 +17,59 @@ sp = Spotify(auth_manager=SpotifyOAuth())
 album_details = sp.new_releases(country="IN", limit=2)
 album_details = album_details["albums"]["items"]
 # x = sp.album_tracks()
-album_info = namedtuple("Song", ["name", "artist", "album_art"])
-album_list: List[album_info] = []
+
+# defining the structure for our albums to be parsed
+# Album = namedtuple("Album", ["name", "artist", "album_art"])
+@dataclass
+class Album:
+    """dataclass that defines the Album object"""
+
+    name: str
+    artist: str
+    album_art_url: str
+
+
+# defining the type makes extracting data much easier when looping
+albums_list: list[Album] = []
 
 for _, entry in enumerate(album_details):
     keys = entry.keys() if keys == [] else keys
-    # break
     name = entry["name"]
     artist = entry["artists"][0]["name"]
-    album_art = entry["images"][0]["url"]
-    album_list.append(album_info(name, artist, album_art))
+    album_art_url = entry["images"][0]["url"]
+    # albums_list.append(Album(name, artist, album_art))
+    a = Album(name, artist, album_art_url)
 
-for _, entry in enumerate(album_list):
+    albums_list.append(a)
+
+for _, entry in enumerate(albums_list):
     print(f"{entry.artist}: {entry.name}")
+
+# with albums aspect complete, we can look to extract data from the song URI
+@dataclass
+class Song:
+    """dataclass that defines the Song object"""
+
+    name: str
+    artist: list
+    album_art_url: str
+
+
+# the "track" method can take in track ID, URI or URL; it is very flexible
+song_details = sp.track(
+    "https://open.spotify.com/track/15aRuSQrMg9pFZmWSaeSgr?si=fa8e32e6fb3547f3"
+)
+# print(song_details.keys())
+
+# there can be multiple artists, iterating over the list and extracting data
+artists = []
+for artist in song_details["artists"]:
+    artists.append(artist["name"])
+
+song = Song(
+    song_details["name"],
+    artists,
+    song_details["album"]["images"][0]["url"],
+)
+
+print(f"\n{', '.join(song.artist)}: {song.name}")
