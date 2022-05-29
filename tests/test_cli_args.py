c7e6b9a354680edfa190ae60b8fdeb357306629e
Dhruv <83733638+dhruv-ahuja@users.noreply.github.com>
2022-05-29 12:47:26 +0530

    feat: update cli args tests
    - add more comments and docstrings for clarity
    
    - split test_defaults test into 2 separate tests

--- tests/test_cli_args.py
+++ tests/test_cli_args.py
@@ -5,11 +5,18 @@ import pytest
 from spotidl import __version__
 
 
+# using this fixture since argument parser will be required by all test
+# functions; by returning a function that generates a parser, we can use
+#  this fixture for many functions
 @pytest.fixture()
 def generate_parser():
+    """
+    Acts as the point from where test functions can retrieve the
+    ArgumentParser mock object to conduct testing.
+    """
+
     def make_parser():
         parser = argparse.ArgumentParser()
-
         return parser
 
     return make_parser
@@ -34,21 +41,31 @@ def test_version(capsys, generate_parser):
 
     try:
         parser.parse_args(["-v"])
-        assert parser.parse_args().version == current_version
 
+    # our cli application performs a SystemExit when called without the 'link'
+    # argument. in case of the version flag, it prints out the current version
+    # and exits, so we need to catch the SystemExit Exception
     except SystemExit:
+        # capsys allows us to capture stdout/stderr
         captured = capsys.readouterr()
         assert captured.out == f"{current_version}\n"
 
 
-def test_defaults(generate_parser):
+def test_defaults_quiet_modified(generate_parser):
+    """
+    Tests the applications' default generated argument values, with the quiet
+    flag having been modified to be `True`.
+    """
+
     parser = generate_parser()
+    # the Namespace class stores the attributes added to the CLI application,
+    # allowing us to mock the expected behaviour easily
     expected_output = argparse.Namespace(
         quiet=True, codec=" mp3", bitrate=" 320", dir=" /dl"
     )
 
     # since we're manually adding the command line args, we have to account for
-    # the space in added after each argument hence the extra space in each
+    # the space added after each argument hence the extra space in each
     # 'default' parameter
     parser.add_argument("-q", "--quiet", action="store_true")
     parser.add_argument("-c", "--codec", default=" mp3")
@@ -56,7 +73,9 @@ def test_defaults(generate_parser):
     parser.add_argument("-d", "--dir", default=" /dl")
 
     try:
-        assert parser.parse_args(["-q", "-c mp3", "-b 320", "-d /dl"])
+        assert (
+            parser.parse_args(["-q", "-c mp3", "-b 320", "-d /dl"]) == expected_output
+        )
 
     except SystemExit:
         # adding a 'pass' here since the argparse module runs,
@@ -64,6 +83,21 @@ def test_defaults(generate_parser):
         # without an except block here, our tests will keep failing
         pass
 
+
+def test_defaults_quiet_unmodified(generate_parser):
+    """
+    Tests the applications' default generated argument values, with the quiet
+    flag staying at its default `False`.
+    """
+
+    parser = generate_parser()
+    parser.add_argument("-q", "--quiet", action="store_true")
+    parser.add_argument("-c", "--codec", default=" mp3")
+    parser.add_argument("-b", "--bitrate", default=" 320")
+    parser.add_argument("-d", "--dir", default=" /dl")
+
+    # checking another variant where we don't modify the quiet param at all
+    # and the rest remain the same
     expected_output = argparse.Namespace(
         quiet=False, codec=" mp3", bitrate=" 320", dir=" /dl"
     )
