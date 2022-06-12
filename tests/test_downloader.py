339a8077361bf721d355f850dccaa7f63ff22cec
Dhruv <83733638+dhruv-ahuja@users.noreply.github.com>
2022-06-12 14:49:29 +0530

    refactor: test_downloader module
    - remove resource-heavy integration tests that downloaded files
    
    - refactor test for downlodaer to compare a mock object instead of
    downloading an actual song
    
    - update get_config fixture with new parameters that
    were added to the core app

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -1,14 +1,20 @@
+import os
+
+
 from yt_dlp import YoutubeDL
 import pytest
 
-import os
 
 import spotidl.downloader as package
-from tests.test_spotify import generate_new_song
+from tests.test_spotify import generate_new_song as spotify_song
 
 
 @pytest.fixture()
 def generate_config():
+    """
+    Generates the config needed to test the YoutubeDL object.
+    """
+
     downloader_params = {
         "postprocessors": [
             {
@@ -21,13 +27,21 @@ def generate_config():
         "quiet": True,
         "format": "bestaudio/best",
         "dynamic_mpd": False,
+        "noplaylist": True,
+        "prefer_ffmpeg": True,
     }
 
-    return downloader_params
+    yield downloader_params
+    del downloader_params
 
 
-def test_get_config(generate_new_song, generate_config):
-    song = generate_new_song
+def test_get_config(spotify_song, generate_config):
+    """
+    Tests to ensure that the config for the YoutubeDL object is generated
+    correctly.
+    """
+
+    song = spotify_song
     user_params = {"quiet": True, "codec": "mp3", "quality": "320"}
 
     expected_output = generate_config
@@ -36,6 +50,10 @@ def test_get_config(generate_new_song, generate_config):
 
 
 def test_get_downloader(generate_config):
+    """
+    Tests the creation of the download object.
+    """
+
     params = generate_config
 
     assert isinstance(package.get_downloader(params), YoutubeDL)
@@ -43,108 +61,78 @@ def test_get_downloader(generate_config):
 
 @pytest.fixture()
 def generate_downloader(generate_config):
-    # we have hard coded one particular name in the config just for testing,
-    # we keep the name dynamic in the actual app
+    """
+    Generates the YoutubeDL object to be used for further testing.
+    """
+
+    # we have hard coded one particular song name in the config just for
+    # testing; we keep the name dynamic in the actual app
     return YoutubeDL(generate_config)
 
 
 @pytest.fixture()
-def generate_yt_song(generate_downloader):
-    yt = generate_downloader
+def generate_yt_song():
+    """
+    Generates a valid YoutubeSong object to be used for testing.
+    """
 
-    def make_yt_song(song_title: str):
-        search = yt.extract_info(f"ytsearch: {song_title}", download=False)
-        yt_info = search["entries"][0]
+    song_id = "pkB9fm08IDM"
+    song_title = "Le sort de Circé"
+    audio_source_url = "https://www.youtube.com/watch?v=pkB9fm08IDM"
 
-        yt_song = package.YoutubeSong(
-            id=yt_info["id"],
-            title=yt_info["title"],
-            video_url=yt_info["webpage_url"],
-        )
+    song = package.YoutubeSong(song_id, song_title, audio_source_url)
 
-        return yt_song
-
-    return make_yt_song
+    yield song
+    del song
 
 
-def test_fetch_source(generate_downloader, generate_yt_song, generate_new_song):
-    song = generate_new_song
-    song_title = "Juliette - Le sort de Circé audio"
-    yt = generate_downloader
-    expected_output = generate_yt_song(song_title)
+def test_fetch_source(generate_downloader, generate_yt_song, spotify_song):
+    # need these objects for the actual function that we're testing
+    song = spotify_song
+    ytd = generate_downloader
 
-    assert package.fetch_source(yt, song) == expected_output
+    expected_output = generate_yt_song
+
+    assert package.fetch_source(ytd, song) == expected_output
 
 
 @pytest.fixture()
 def make_test_dir(tmp_path):
-    d = tmp_path / "test"
-    d.mkdir()
+    """
+    Creates a mock test directory.
+    """
 
-    return d
+    directory = tmp_path / "test"
+    directory.mkdir()
 
-
-def test_download_song(generate_downloader, generate_yt_song):
-    yt = generate_downloader
-    song_title = "Juliette - Le sort de Circé"
-    yt_song = generate_yt_song(song_title)
-
-    # changing directory to the app's default downloads folder
-    os.makedirs("./dl", exist_ok=True)
-    os.chdir("./dl")
-
-    try:
-        package.download_song(yt, yt_song.video_url)
-
-    except Exception:
-        assert not os.path.isfile(f"./{song_title}.mp3")
-
-    else:
-
-        assert os.path.isfile(f"./{song_title}.mp3")
+    # yielding the directory and then regaining control when function execution
+    # ends will enable to us to remove it from the memory
+    yield directory
+    del directory
 
 
-def test_controller_mp3(generate_new_song, capsys):
-    song = generate_new_song
-    file_name = "Juliette - Le sort de Circé.mp3"
-    user_params = {"codec": "mp3", "quality": "320", "quiet": True}
+@pytest.fixture()
+def make_test_song(make_test_dir):
+    """
+    Creates a mock song file.
+    """
 
-    # since we already downloaded the song in the previous test, we will first get to test the first part of the controller
-    expected_output = f"\n{file_name} already exists! Skipping download...\n"
+    def make_song(codec: str = "mp3"):
+        song_file = make_test_dir / f"artist - unknown.{codec}"
+        song_file.write_text("testing")
 
-    # changing directory to the app's default downloads folder
-    os.makedirs("./dl", exist_ok=True)
-    os.chdir("./dl")
-    package.controller(user_params, song, file_name)
-    captured = capsys.readouterr()
+        return song_file
 
-    assert captured.out == expected_output
-
-    # now testing the else statement part
-    os.remove(f"./{file_name}")
-
-    package.controller(user_params, song, file_name)
-
-    assert os.path.isfile(f"./{file_name}")
-
-    os.chdir("..")
+    yield make_song
+    del make_song
 
 
-def test_controller_flac(generate_new_song):
-    song = generate_new_song
-    file_name = "Juliette - Le sort de Circé.flac"
-    user_params = {"codec": "flac", "quality": "320", "quiet": True}
+def test_download_song(make_test_song):
+    """
+    Mocks the behaviour of the download_song function.
+    """
 
-    # changing directory to the app's default downloads folder
-    os.makedirs("./dl", exist_ok=True)
-    os.chdir("./dl")
-    package.controller(user_params, song, file_name)
+    # mocking the behaviour of the downloader function
+    downloaded_song = make_test_song()
 
-    # now testing the else statement part
-    os.remove(f"./{file_name}")
-
-    package.controller(user_params, song, file_name)
-
-    assert os.path.isfile(f"./{file_name}")
-
-    os.chdir("..")
+    assert os.path.isfile(downloaded_song)
