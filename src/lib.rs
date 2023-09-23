b9dd3287dad3347c8bcef1d2dfa5daccf1140095
Dhruv Ahuja <dhruvahuja2k@gmail.com>
2023-09-23 17:20:48 +0530

    feat[rust]: generate spotify client function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
+use std::time::{Duration, Instant};
+
 use pyo3::prelude::*;
+use pyo3::types;
+use rspotify::prelude::*;
+use rspotify::AuthCodeSpotify;
+use tokio::time::sleep;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -11,4 +17,13 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 fn spotidl_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
+}
+
+fn generate_client(token: String) -> AuthCodeSpotify {
+    let access_token = rspotify::Token {
+        access_token: token,
+        expires_in: chrono::Duration::seconds(3600),
+        ..Default::default()
+    };
+    rspotify::AuthCodeSpotify::from_token(access_token)
 }
