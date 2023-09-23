37113d2516ddbcc6fc5177dd8c000eeeef477ce8
Dhruv Ahuja <dhruvahuja2k@gmail.com>
2023-09-23 17:19:39 +0530

    chore: make basic project changes
    - add required rust dependencies
    - bump project version in toml files to 2.0

--- spotidl/__init__.py
+++ spotidl/__init__.py
@@ -1,6 +1,6 @@
 from importlib import metadata
 
-__version__ = metadata.version("spoti-dl")
 
+__version__ = metadata.version("spoti-dl")
 
 del metadata  # optional, to avoid polluting the results of dir(__package__)
