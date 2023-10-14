6e0cc28a7383a6e43f59c4705edaa6ec450e5f30
Dhruv Ahuja <83733638+dhruv-ahuja@users.noreply.github.com>
2023-10-14 20:48:50 +0530

    chore: remove uneeded cli python function

--- spotidl/main.py
+++ spotidl/main.py
@@ -3,15 +3,11 @@ import asyncio
 from spotidl.cli import controller
 
 
-async def cli():
-    await controller()
-
-
 def main():
     event_loop = asyncio.get_event_loop()
-    task = event_loop.create_task(cli())
+    task = event_loop.create_task(controller())
     event_loop.run_until_complete(task)
 
 
 if __name__ == "__main__":
-    asyncio.run(cli())
+    main()
